31367ae67075ef6dc7cd9be4488d689301945400
BeastLe9enD <beastle9end@outlook.de>
2023-08-02 22:09:40 +0200

    ✨ Add read functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-use std::{future::Future, io::ErrorKind, mem, pin::Pin, task::{Context, Poll}};
-use byteorder::ByteOrder;
+use std::{
+    future::Future,
+    io::ErrorKind,
+    marker::PhantomData,
+    mem,
+    pin::Pin,
+    task::{Context, Poll},
+};
 
-use futures_lite::{io, ready, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use byteorder::ByteOrder;
+use futures_lite::{io, ready, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 macro_rules! write_future_8 {
     ($future: ident, $ty: ty) => {
@@ -33,7 +40,7 @@ macro_rules! write_future {
         pub struct $future<'a, W: Unpin + ?Sized> {
             writer: &'a mut W,
             buf: [u8; mem::size_of::<$ty>()],
-            n: usize
+            n: usize,
         }
 
         impl<W: AsyncWrite + Unpin + ?Sized> Future for $future<'_, W> {
@@ -46,32 +53,46 @@ macro_rules! write_future {
                     *n = ready!(Pin::new(&mut **writer).poll_write(cx, &buf[*n..]))?;
 
                     if *n == 0 {
-                        return Poll::Ready(Err(ErrorKind::WriteZero.into()));
+                        return Poll::Ready(Err(ErrorKind::WriteZero.into()))
                     }
                 }
 
                 Poll::Ready(Ok(()))
             }
         }
-    }
+    };
+}
+
+macro_rules! write_impl_8 {
+    ($future: ident, $name: ident, $ty: ty) => {
+        fn $name(&mut self, value: $ty) -> $future<'_, Self>
+        where
+            Self: Unpin,
+        {
+            $future {
+                writer: self,
+                buf: [value as u8],
+            }
+        }
+    };
 }
 
 macro_rules! write_impl {
     ($future: ident, $name: ident, $ty: ty) => {
-        fn $name<T: ByteOrder>(&mut self, value: $ty) -> $future<Self>
+        fn $name<T: ByteOrder>(&mut self, value: $ty) -> $future<'_, Self>
         where
-            Self: Unpin
+            Self: Unpin,
         {
             let mut future = $future {
                 writer: self,
                 buf: [0; mem::size_of::<$ty>()],
-                n: 0
+                n: 0,
             };
             T::$name(&mut future.buf, value);
 
             future
         }
-    }
+    };
 }
 
 write_future_8!(WriteU8Future, u8);
@@ -85,25 +106,8 @@ write_future!(WriteU64Future, u64);
 write_future!(WriteI64Future, i64);
 
 pub trait AsyncByteOrderWrite: AsyncWriteExt {
-    fn write_u8(&mut self, value: u8) -> WriteU8Future<Self>
-    where
-        Self: Unpin,
-    {
-        WriteU8Future {
-            writer: self,
-            buf: [value],
-        }
-    }
-
-    fn write_i8(&mut self, value: i8) -> WriteI8Future<Self>
-    where
-        Self: Unpin,
-    {
-        WriteI8Future {
-            writer: self,
-            buf: [value as _]
-        }
-    }
+    write_impl_8!(WriteU8Future, write_u8, u8);
+    write_impl_8!(WriteI8Future, write_i8, i8);
 
     write_impl!(WriteU16Future, write_u16, u16);
     write_impl!(WriteI16Future, write_i16, i16);
@@ -113,22 +117,108 @@ pub trait AsyncByteOrderWrite: AsyncWriteExt {
     write_impl!(WriteI64Future, write_i64, i64);
 }
 
-pub struct ReadU8Future {
-
-}
-
 macro_rules! read_future_8 {
+    ($future: ident, $ty: ty) => {
+        pub struct $future<'a, R: Unpin + ?Sized> {
+            reader: &'a mut R,
+            buf: [u8; 1],
+        }
 
+        impl<R: AsyncRead + Unpin + ?Sized> Future for $future<'_, R> {
+            type Output = io::Result<$ty>;
+
+            fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+                let Self { reader, buf } = &mut *self;
+
+                let n = ready!(Pin::new(&mut *reader).poll_read(cx, buf))?;
+
+                if n == 0 {
+                    return Poll::Ready(Err(ErrorKind::UnexpectedEof.into()))
+                }
+
+                Poll::Ready(Ok((buf[0] as $ty)))
+            }
+        }
+    };
 }
 
 macro_rules! read_future {
+    ($future: ident, $name: ident, $ty: ty) => {
+        pub struct $future<'a, R: Unpin + ?Sized, T: ByteOrder> {
+            reader: &'a mut R,
+            buf: [u8; mem::size_of::<$ty>()],
+            n: usize,
+            phantom: PhantomData<&'a T>
+        }
 
+        impl<R: AsyncRead + Unpin + ?Sized, T: ByteOrder> Future for $future<'_, R, T> {
+            type Output = io::Result<$ty>;
+
+            fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+                let Self { reader, buf, n, .. } = &mut *self;
+
+                while *n < buf.len() {
+                    *n = ready!(Pin::new(&mut **reader).poll_read(cx, &mut buf[*n..]))?;
+
+                    if *n == 0 {
+                        return Poll::Ready(Err(ErrorKind::UnexpectedEof.into()))
+                    }
+                }
+
+                Poll::Ready(Ok(T::$name(buf)))
+            }
+        }
+    };
+}
+
+macro_rules! read_impl_8 {
+    ($future: ident, $name: ident, $ty: ty) => {
+        fn $name(&mut self) -> $future<'_, Self>
+        where
+            Self: Unpin,
+        {
+            $future {
+                reader: self,
+                buf: Default::default(),
+            }
+        }
+    };
 }
 
 macro_rules! read_impl {
-
+    ($future: ident, $name: ident, $ty: ty) => {
+        fn $name<T: ByteOrder>(&mut self) -> $future<'_, Self, T>
+        where
+            Self: Unpin,
+        {
+            $future {
+                reader: self,
+                buf: Default::default(),
+                n: 0,
+                phantom: PhantomData
+            }
+        }
+    }
 }
 
-pub trait AsyncByteOrderReader: AsyncReadExt {
+read_future_8!(ReadU8Future, u8);
+read_future_8!(ReadI8Future, i8);
 
+read_future!(ReadU16Future, read_u16, u16);
+read_future!(ReadI16Future, read_i16, i16);
+read_future!(ReadU32Future, read_u32, u32);
+read_future!(ReadI32Future, read_i32, i32);
+read_future!(ReadU64Future, read_u64, u64);
+read_future!(ReadI64Future, read_i64, i64);
+
+pub trait AsyncByteOrderReader: AsyncReadExt {
+    read_impl_8!(ReadU8Future, read_u8, u8);
+    read_impl_8!(ReadI8Future, read_i8, i8);
+
+    read_impl!(ReadU16Future, read_u16, u16);
+    read_impl!(ReadI16Future, read_i16, i16);
+    read_impl!(ReadU32Future, read_u32, u32);
+    read_impl!(ReadI32Future, read_i32, i32);
+    read_impl!(ReadU64Future, read_u64, u64);
+    read_impl!(ReadI64Future, read_i64, i64);
 }
